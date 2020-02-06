e5987bc2de5e4598fb07a2e08d3f96db995ca154
Antonio <tonialcantara1997@gmail.com>
2020-02-06 19:50:08 +0100

    Add mapstruck and order versions of maven into properties and change service find events to eventsDto

--- src/main/java/com/organize/app/controllers/EventController.java
+++ src/main/java/com/organize/app/controllers/EventController.java
@@ -1,6 +1,7 @@
 package com.organize.app.controllers;
 
-import com.organize.app.models.entity.Event;
+import com.organize.app.models.dto.EventDto;
+import com.organize.app.models.mapper.IEventMapper;
 import com.organize.app.models.service.IEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,11 @@ import java.util.List;
 @RestController("/v1/event")
 public class EventController {
 
-    @Autowired
-    private IEventService eventService;
+    @Autowired private IEventMapper eventMapper;
+    @Autowired private IEventService eventService;
 
     @GetMapping("/find/all")
-    public List<Event> getAllEvents() {
-        return eventService.findAll();
+    public List<EventDto> getAllEvents() {
+        return eventMapper.toDtos(eventService.findAll());
     }
 }
--- src/main/java/com/organize/app/models/entity/EventType.java
+++ src/main/java/com/organize/app/models/entity/EventType.java
@@ -1,6 +1,7 @@
 package com.organize.app.models.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -14,6 +15,8 @@ public class EventType implements Serializable {
     @Id
     private String type;
 
+    @Transient
+    @ToString.Exclude
     @OneToMany(mappedBy = "eventType", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Event> events;
 }
