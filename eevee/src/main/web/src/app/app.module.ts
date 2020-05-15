133017546afd137854b8fdc7afd429215dfca9f9
Sylvia Marín <sylviamarin@mbp-de-sylvia.home>
2020-05-15 09:58:07 +0200

    create register component

--- eevee/src/main/web/src/app/app.module.ts
+++ eevee/src/main/web/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatButtonModule} from '@angular/material/button';
 import { MatIconModule} from '@angular/material/icon';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
