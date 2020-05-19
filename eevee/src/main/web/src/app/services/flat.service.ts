417827bbb2b5736bd37eb58dcb3dfb4b346653d7
Antonio <tonialcantara1997@gmail.com>
2020-05-19 22:07:32 +0200

    add methods for consume flat ws

--- eevee/src/main/web/src/app/services/flat.service.ts
+++ eevee/src/main/web/src/app/services/flat.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { CreateFlatModel } from '../models/create-flat.model';
+import { EventModel } from '../models/event.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlatService {
 
-  constructor() { }
+  host = `${environment.host}v1/flat`;
+
+  constructor(private http: HttpClient) { }
+
+  addUsersToFlat(flatId: number, userIds: number[]): Observable<HttpResponse<void>> {
+    const url = `${this.host}/${flatId}/add/user`;
+    return this.http.put<void>(url, userIds, {observe: 'response'});
+  }
+
+  createFlat(flatModel: CreateFlatModel): Observable<HttpResponse<void>> {
+    const url = `${this.host}/create`;
+    return this.http.post<void>(url, flatModel, {observe: 'response'});
+  }
+
+  findEventsByFlat(flatId: number): Observable<HttpResponse<EventModel[]>> {
+    const url = `${this.host}/find/event/${flatId}`;
+    return this.http.get<EventModel[]>(url, {observe: 'response'});
+  }
 }
