4f203f00003241c64c815dcf11f9df2b44f618b9
Antonio <tonialcantara1997@gmail.com>
2020-05-19 21:59:25 +0200

    Add methods for consume room ws

--- eevee/src/main/web/src/app/services/room.service.ts
+++ eevee/src/main/web/src/app/services/room.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { RoomModel } from '../models/room.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  host = `${environment.host}v1/room`;
+
+  constructor(private http: HttpClient) { }
+
+  createRoom(name: string, locale: string): Observable<HttpResponse<void>> {
+    const url = `${this.host}/create/${name}/${locale}`;
+    return this.http.post<void>(url, null, {observe: 'response'});
+  }
+
+  findAllRooms(locale: string): Observable<HttpResponse<RoomModel[]>> {
+    const url = `${this.host}/findALL/${locale}`;
+    return this.http.get<RoomModel[]>(url, {observe: 'response'});
+  }
 }
--- eevee/src/main/web/src/environments/environment.prod.ts
+++ eevee/src/main/web/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  host: 'http://51.210.46.242:8084/'
 };
